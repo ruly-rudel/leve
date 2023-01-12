dc2ba0722aa60b94cf29c748aa6457adc36e11e1
Ruly Rudel <ruly.rudel@gmail.com>
2023-01-12 20:32:29 +0900

    testbench environment is updated.

--- sim/run.sh
+++ sim/run.sh
@@ -8,8 +8,9 @@ fi
 
 rm iss.log
 for file_name in "${files[@]}"; do
-	rm rv64ui-p.hex
+	rm rv64ui-p.hex rv64ui-p.dump
 	ln -s ${file_name} rv64ui-p.hex
+	ln -s ${file_name/hex/dump} rv64ui-p.dump
 	echo "[TESTBENCH] execute test ${file_name}" >> iss.log
 	./obj_dir/Vtb_iss >> iss.log
 done
